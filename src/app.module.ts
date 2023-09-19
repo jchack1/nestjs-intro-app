5e37b3983e82ea876cb65aff61a53cd962428610
jchack1 <55161597+jchack1@users.noreply.github.com>
2023-09-18 19:37:13 -0600

    update ignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 // import { ItemsService } from './items/items.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ItemsModule } from './items/items.module';
-import config from './config/keys';
+import config from '../config/keys';
 
 @Module({
   imports: [ItemsModule, MongooseModule.forRoot(config.mongoURI)], // uri string
--- src/config/keys.ts
+++
@@ -1,4 +0,0 @@
-export default {
-  mongoURI:
-    'mongodb+srv://jchack:oHZoax3X8wiBExS7@cluster0-hkg5c.mongodb.net/nestIntro?retryWrites=true&w=majority',
-};
